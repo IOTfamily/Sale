69f4eaf875f1c3b84fc58b3ecb2a22e11bbb3a8a
luozhanwei <luozhanwei@xiaomi.com>
2017-12-31 12:02:33 +0800

    获取商品列表

---
+++ src/main/java/com/iot/sale/Service/service/GoodListService.java
@@ -0,0 +1,11 @@
+package com.iot.sale.Service.service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by luxiaolin on 17/12/31.
+ */
+public interface GoodListService {
+    public List<Map<String, Object>> getGoodListInHome();
+}
