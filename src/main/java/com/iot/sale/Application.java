87740a839f333d5a1e45563ada72c233309510b5
颜回 <yanhui@51huxin.com>
2018-01-03 10:05:48 +0800

    redis配置

--- src/main/java/com/iot/sale/Application.java
+++ src/main/java/com/iot/sale/Application.java
@@ -3,8 +3,12 @@ package com.iot.sale;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan(basePackages = "com.iot")
+@ComponentScan(basePackages = {"com.iot"})
 //@EnableWebSecurity
 public class Application {
 
